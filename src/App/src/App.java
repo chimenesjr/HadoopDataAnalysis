b8ed73ba1430b9a352973e90d34273bd6d051e55
chimenesjr <chimenesjr@gmail.com>
2021-03-26 09:40:41 +0000

    files

--- src/App/src/App.java
+++ src/App/src/App.java
@@ -2,10 +2,7 @@ package HDA;
 
 import java.io.*;
 import java.util.*;
-import java.util.Random;
-import java.text.*;
 
-import HDA.MapIGTI;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.*;
@@ -23,49 +20,27 @@ public class App extends Configured implements Tool
     public int run (final String[] args) throws Exception {
       try{ 	             	       	
             JobConf conf = new JobConf(getConf(), App.class);			
-            conf.setJobName("Exemplo IGTI - Media");            
 
             final FileSystem fs = FileSystem.get(conf); 
-            Path diretorioEntrada = new Path("Entrada" + UUID.randomUUID().toString()), diretorioSaida = new Path("Saida" + UUID.randomUUID().toString());
-            /* Criar um diretorio de entrada no HDFS */
+            Path diretorioEntrada = new Path("DataIn" + UUID.randomUUID().toString()), diretorioSaida = new Path("DataOut" + UUID.randomUUID().toString());
+
             if (!fs.exists(diretorioEntrada))
                 fs.mkdirs(diretorioEntrada);
-            /* Adicionar um arquivo para ser processado */
-            fs.copyFromLocalFile(new Path("/usr/local/hadoop/Dados/arquivoBigData.txt"), diretorioEntrada);             
-            /* Atribuindo os diretorios de Entrada e Saida para o Job */
-            FileInputFormat.setInputPaths(conf,  diretorioEntrada); 
+
+            fs.copyFromLocalFile(new Path("/usr/local/hadoop/App/data/PPR-ALL.csv"), diretorioEntrada);
+
+            FileInputFormat.setInputPaths(conf,  diretorioEntrada);
             FileOutputFormat.setOutputPath(conf, diretorioSaida);
   
             conf.setOutputKeyClass(Text.class);     
             conf.setOutputValueClass(Text.class);   
-            conf.setMapperClass(MapIGTI.MapIGTIMap.class);
-            conf.setReducerClass(ReduceIGTI.class);
+            conf.setMapperClass(HDA.JobQuantitativo.Map.class);
+            conf.setReducerClass(HDA.JobQuantitativo.Reduce.class);
             JobClient.runJob(conf);   
-                                          
         }
         catch ( Exception e ) {
             throw e;
         }
         return 0;
-     }
- 
-
-    public static class ReduceIGTI extends MapReduceBase implements Reducer<Text, Text, Text, Text> {       
-      
-       public void reduce (Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {                                                                                 double media = 0.0; 
-            int acumuladorItens = 0, contaVendas = 0;
-            Text value = new Text();
-        
-            while (values.hasNext()) {
-                value = values.next();               
-                contaVendas++;
-                acumuladorItens += Integer.parseInt(value.toString());
-            }            
-            media = acumuladorItens / new Double(contaVendas);
-                          
-            value.set(String.valueOf(media)); 
-            output.collect(key, value);           
-      }            
-    
     }
 }
