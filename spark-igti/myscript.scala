280673ba22d0f0d359e55676b26257775040bfae
chimenesjr <chimenesjr@gmail.com>
2021-05-08 11:05:20 +0100

    fix

--- spark-igti/myscript.scala
+++ spark-igti/myscript.scala
@@ -11,7 +11,7 @@ object MPRApp {
       val arquivo = sc.textFile("file:///usr/local/HadoopDataAnalysis/spark-igti/arquivoBigData.txt")    
       
       val clientes_vendas = arquivo.map(linha => (linha.substring(58, 61).toInt, linha.substring(76, 84).toDouble)) 
-      val grupo = clientes_vendas.reduceByKey((x, y) => (x+))
+      val grupo = clientes_vendas.reduceByKey((x, y) => (x+y))
       val grupo_ord = grupo.sortByKey()  
       
       grupo_ord.saveAsTextFile("file:///usr/local/HadoopDataAnalysis/spark-igti/resultado");
