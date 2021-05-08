75a8b9bab2453a933c89a83700f177d328ad59fd
chimenesjr <chimenesjr@gmail.com>
2021-05-08 08:16:56 +0100

    test

--- spark-igti/myscript.scala
+++ spark-igti/myscript.scala
@@ -4,15 +4,17 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object MPRApp {
    def main(args: Array[String]) {   
-        var conf = new SparkConf().setAppName("IGTI MPRApp")
-        var sc = new SparkContext(conf)
-        
-        val arquivo = sc.textFile("hdfs://localhost:54310/entrada/arquivoBigData.txt")    
-        
-        val clientes_vendas = arquivo.map(linha => (linha.substring(58, 61).toInt, linha.substring(76, 84).toDouble)) 
-        val grupo = clientes_vendas.reduceByKey((x, y) => (x+y))
-        val grupo_ord = grupo.sortByKey()  
-       
-        grupo_ord.saveAsTextFile("hdfs://localhost:54310/entrada/resultado");    
+      var conf = new SparkConf().setAppName("IGTI MPRApp")
+      var sc = new SparkContext(conf)
+      
+      // val arquivo = sc.textFile("hdfs://localhost:54310/entrada/arquivoBigData.txt")    
+      
+      // val clientes_vendas = arquivo.map(linha => (linha.substring(58, 61).toInt, linha.substring(76, 84).toDouble)) 
+      // val grupo = clientes_vendas.reduceByKey((x, y) => (x+y))
+      // val grupo_ord = grupo.sortByKey()  
+      
+      // grupo_ord.saveAsTextFile("hdfs://localhost:54310/entrada/resultado");
+
+      Console.printf("First scala test")
    }
 }
