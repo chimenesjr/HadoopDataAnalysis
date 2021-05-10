4945a9449f124c6c7e8d89e0f1882b0df6ffcc48
chimenesjr <chimenesjr@gmail.com>
2021-05-10 18:30:11 +0100

    job

--- Spark/main.scala
+++ Spark/main.scala
@@ -3,7 +3,7 @@ package ReportJobs
 
 import org.apache.spark.{SparkContext, SparkConf}
 // import TraitJob
-// import JobMedia
+import JobMedia
 
 object ReportJobs {
     def main(args: Array[String]) {   
@@ -11,7 +11,7 @@ object ReportJobs {
         var conf = new SparkConf().setAppName("Report Jobs")
         var sc = new SparkContext(conf)
 
-        // var jobmedia = new JobMedia
+        var jobmedia = new JobMedia()
         // jobmedia.Start(conf, sc)
     }
 }
