0fdfe9a6531aef8e062f94f1c5df23e127641680
chimenesjr <chimenesjr@gmail.com>
2021-05-19 08:06:28 +0100

    media

--- Python/jobmedia.py
+++ Python/jobmedia.py
@@ -6,14 +6,14 @@ from pyspark.sql.functions import col,array_contains
 
 class jobmediaclass:
 
-    def __init__(self, fileData):
-        self.file = fileData
+    def __init__(self, dataFile):
+        self.dataFile = dataFile
 
     def start(self):
         spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
-        rdd = spark.sparkContext.textFile(self.file).repartition(1)
+        rdd = spark.sparkContext.textFile(self.dataFile).repartition(1)
 
-        line = rdd.map(lambda x: x[1:-1].split("\",\"")) 
+        line = rdd.map(lambda x: x[1:-1].split("\",\""))
         #['01/01/2010', '5 Braemor Drive, Churchtown, Co.Dublin', '', 'Dublin', '343,000.00', 'No', 'No', 'Second-Hand Dwelling house /Apartment', '']
         
         groupedByCity = line.map(lambda x: (x[0][-4:] + ":" + x[3], x[4].replace(",","") + ":1")) 
@@ -22,9 +22,26 @@ class jobmediaclass:
         sum = groupedByCity.reduceByKey(lambda a, b: str(float(a.split(":")[0])+float(b.split(":")[0])) + ":" + str(int(a.split(":")[1])+int(b.split(":")[1])))
         # ('2011:Dublin', '1825609529.210002:5904')
 
-        result = sum.map(lambda x: (x[0].split(":")[0], x[0].split(":")[1], float(x[1].split(":")[0])/int(x[1].split(":")[1])))
+        result = sum.map(lambda x: (int(x[0].split(":")[0]), x[0].split(":")[1], float(x[1].split(":")[0])/int(x[1].split(":")[1])))
         #('2010', 'Dublin', 332967.37502378575)
 
         result.saveAsTextFile("file:///usr/local/jobmedia")
 
+        rdd2010 = result.sortBy(lambda x: x[1]).filter(lambda x: x[0] == 2010).collect()
+        rdd2021 = result.sortBy(lambda x: x[1]).filter(lambda x: x[0] == 2021).collect()
+        # ('2021', 'Dublin', 531246.3120730022)
 
+        avg = []
+        i = 0
+        while i < len(rdd2010):
+            curr = avgResult(rdd2010[i][0], rdd2010[i][1])
+            curr.diff = rdd2021[i][2] - rdd2010[i][2]
+
+            avg.append(curr)
+            i += 1
+
+
+class avgResult:
+    def __init__(self, year, city):
+        self.year = year
+        self.city = city
